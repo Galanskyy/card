09e5f308b69c4a2d488996cf6528d85fbb8f9170
Galanskyy <42960637+Galanskyy@users.noreply.github.com>
2020-10-21 11:01:56 +0200

    animacje i guzik kup

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ content.addEventListener('mouseleave', function(){
     desc.style.display = "none";
 } );
 
+
+const btnBuy = document.querySelector(".buy");
+const price = document.querySelector(".price");
+const btnClose = document.querySelector(".close");
+const after = document.querySelector(".buy-after");
+
+btnBuy.addEventListener("click", function(){
+    after.style.animation = "slidebot 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
+    after.style.display = "flex";
+    btnBuy.style.zIndex = "0";
+    price.style.zIndex = "0";
+});
+btnClose.addEventListener("click", function(){
+    after.style.display = "none";
+    btnBuy.style.zIndex = "1";
+    price.style.zIndex = "1";
+});
